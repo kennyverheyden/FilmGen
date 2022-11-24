ceb441aaceb660127fa6dc009f199f309058c4a7
Kenny Verheyden <kenny.verheyden@gmail.com>
2022-11-24 10:21:59 +0100

    Welcome msg + while loop option menu

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,8 @@
 package main;
 
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.Scanner;
 
-import classes.DBConnect;
 
 public class Main {
 
@@ -11,20 +10,66 @@ public class Main {
 		// TODO Auto-generated method stub
 
 
-		// Make DB connection
-		DBConnect myDBConnection = new DBConnect();
+		//		// Make DB connection
+		//		DBConnect myDBConnection = new DBConnect();
+		//
+		//		// DB test
+		//		myDBConnection.openDB();
+		//
+		//		// retrieve films to test
+		//		ArrayList<String> films = myDBConnection.getCategories();
+		//
+		//		// print loaded arrayList
+		//		for(String value: films)
+		//		{
+		//			System.out.println(value);
+		//		}
 
-		// DB test
-		myDBConnection.openDB();
+		Scanner userinput = new Scanner(System.in);
+		boolean exitProgram = false;
+		char userChose=' ';
 
-		// retrieve films to test
-		ArrayList<String> films = myDBConnection.getCategories();
+		showWelcome();
 
-		// print loaded arrayList
-		for(String value: films)
+
+		do
 		{
-			System.out.println(value);
+
+			System.out.println("    [1] Option 1\n");
+			System.out.println("    [2] Option 2 \n");
+			System.out.println(" ");
+			System.out.println("    Press [q] to quit");
+			System.out.println("");
+
+			System.out.print("  Choice: ");
+			userChose=userinput.next().toLowerCase().charAt(0);
+
+			switch(userChose) {
+			case '1':
+				System.out.println("....");
+				break;
+			case '2':
+				System.out.println("....");
+				break;
+			case 'q':
+				exitProgram = true;
+				break;
+			}
+
 		}
+		while(!exitProgram);
+
+
+	}
+
+	public static void showWelcome()
+	{
+		System.out.println("\n  *************************************************************************");
+		System.out.println("  *****                                                               *****");
+		System.out.println("  *****                       Film generator                          *****");
+		System.out.println("  *****                                                               *****");
+		System.out.println("  *************************************************************************");
+		System.out.println(" ");
 	}
 
 }
