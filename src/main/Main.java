1479580706f1fc87d7b0eb4e53455b7a60ca5216
Kenny Verheyden <54863392+kennyverheyden@users.noreply.github.com>
2022-12-18 14:01:35 +0100

    Structure code on Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,38 +1,43 @@
 package main;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-import classes.DBConnect;
 import classes.Film;
 
 public class Main {
 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Scanner userinput = new Scanner(System.in);
-		boolean exitProgram = false;
-		char userChose=' ';
-		Film film = new Film();
 
 		showWelcome();
+		
+		runProgram(); // start content menu
+		
+		showExitMessage();
+
+	}
+
+
+	// Start content menu
+	private static void runProgram()
+	{
+		Scanner userInput = new Scanner(System.in);
+		boolean exitProgram = false;
+		char userChose=' ';
 
 		do
 		{
-			System.out.println("    [1] Generate Film name");
+			System.out.println("    [1] Generate film name");
 			System.out.println("    [2] Stored generated films");
 			System.out.println("    [3] Edit content \n");
 			System.out.println(" ");
 			System.out.println("    Press [q] to quit");
 			System.out.println("");
-
 			System.out.print("  Choice: ");
-			userChose=userinput.next().toLowerCase().charAt(0);
+			userChose=userInput.next().toLowerCase().charAt(0);
 
 			switch(userChose) {
 			case '1':
-				film.buildTitle();
+				filmGenerateMenu();
 				break;
 			case '2':
 				System.out.println("....");
@@ -41,13 +46,53 @@ public class Main {
 				exitProgram = true;
 				break;
 			}
-
 		}
 		while(!exitProgram);
-
 	}
 
-	public static void showWelcome()
+	private static void filmGenerateMenu()
+	{
+		Scanner userInput = new Scanner(System.in);
+		Film film = new Film();
+		char userChose=' ';
+
+		System.out.println("\n    Select a genre\n");
+		System.out.println("    [1] Generate general film name");
+		System.out.println("    [2] Genre 1");
+		System.out.println("    [3] Genre 2 \n");
+		System.out.println(" ");
+		System.out.println("    Press [q] to quit");
+		System.out.println("");
+		System.out.print("  Choice: ");
+
+		userChose=userInput.next().toLowerCase().charAt(0);
+		switch(userChose) {
+		case '1':
+			film.buildTitle();
+			pressKeyToContinue();
+			break;
+
+		case '2':
+			// submenu
+			System.out.print("Genre 1");
+			pressKeyToContinue();
+			break;
+			// end sub
+		case '3':
+			System.out.println("Genre 2");
+			break;
+		}
+	}
+
+	// Pause or stop the program till user input
+	private static void pressKeyToContinue() {
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Press enter to continue");
+		userInput.nextLine();
+	}
+
+	// Show welcome message 
+	private static void showWelcome()
 	{
 		System.out.println("\n  *************************************************************************");
 		System.out.println("  *****                                                               *****");
@@ -57,4 +102,8 @@ public class Main {
 		System.out.println(" ");
 	}
 
+	// Show message when the program closes
+	private static void showExitMessage() {
+		System.out.println("\nThank you for using this program");
+	}
 }
